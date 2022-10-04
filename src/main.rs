9c080374bbffdde5ad01d77c3733d50c43b47f15
karak1974 <wolfy42@protonmail.com>
2022-10-04 15:16:07 +0200

    Added --git, made modular

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,17 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::exit;
 
-const BASE_PATH: &str = "/sys/class/backlight/intel_backlight/";
-const HELP_STR: &str = "communism: tool to control intel backlight\n--help to display this text\n--up <x> to increase brightness by x percent\n--down <x> to decrease brightness by x percent\n";
+// Intel version
+//const BASE_PATH: &str = "/sys/class/backlight/intel_backlight/";
+// AMD version
+const BASE_PATH: &str = "/sys/class/backlight/amdgpu_bl0/";
+const HELP_STR: &str = "communism: tool to control intel backlight\n--help to display this text\n--up <x> to increase brightness by x percent\n--down <x> to decrease brightness by x percent\n--get show the current brightness in percentage\n";
+
+enum Action {
+    Up(f32),
+    Down(f32),
+    Get,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,15 +30,31 @@ fn main() {
     let cur_br_path = Path::new(&(cur_br_path));
     let write_br_path = Path::new(&(write_br_path));
 
-    let up: bool;
     let num: f32;
 
+    let action: Action;
     match args.len() {
+        2 => {
+            action = match args[1].as_ref() {
+                "--get" => Action::Get,
+                _ => {
+                    print!("{}", HELP_STR.to_owned());
+                    exit(0);
+                }
+            }
+        }
         3 => {
-            up = if args[1] == "--up" { true } else { false };
             num = match args[2].parse() {
                 Ok(n) => n,
                 Err(e) => panic!("{}", e),
+            };
+            action = match args[1].as_ref() {
+                "--up" => Action::Up(num),
+                "--down" => Action::Down(num),
+                _ => {
+                    print!("{}", HELP_STR.to_owned());
+                    exit(0);
+                }
             };
         }
 
@@ -64,12 +89,23 @@ fn main() {
 
     let target_br: i32;
 
-    if up {
-        target_br = min(max_br as i32, cur_br + ((max_br * (num / 100.0)) as i32));
-    } else {
-        target_br = max(0, cur_br - ((max_br * (num / 100.0)) as i32));
+    match action {
+        Action::Up(num) => {
+            target_br = min(max_br as i32, cur_br + ((max_br * (num / 100.0)) as i32));
+            write_to_file(target_br, write_br_path);
+        },
+        Action::Down(num) => {
+            target_br = max(0, cur_br - ((max_br * (num / 100.0)) as i32));
+            write_to_file(target_br, write_br_path);
+        },
+        Action::Get => {
+            let percent = (cur_br * 100) / 255;
+            println!("{}%", percent);
+        }
     }
+}
 
+fn write_to_file(target_br: i32, write_br_path: &Path) {
     let mut write_br_file = match File::create(&write_br_path) {
         Err(why) => panic!("couldn't create brightness: {}", why),
         Ok(file) => file,
